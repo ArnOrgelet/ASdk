b13e6b467786657d043547171a06b9d0f4bf25f7
= <=>
2017-05-08 11:37:51 +0200

    on avance..

---
+++ adsdk/src/main/java/sdk/addeals/ahead_solutions/adsdk/Libs/Helpers/DataMapperFactory.java
@@ -0,0 +1,8 @@
+package sdk.addeals.ahead_solutions.adsdk.Libs.Helpers;
+
+/**
+ * Created by ArnOr on 07/05/2017.
+ */
+
+public class DataMapperFactory {
+}
---
+++ adsdk/src/main/java/sdk/addeals/ahead_solutions/adsdk/ViewModels/AdDealsWallViewModel.java
@@ -0,0 +1,8 @@
+package sdk.addeals.ahead_solutions.adsdk.ViewModels;
+
+/**
+ * Created by ArnOr on 07/05/2017.
+ */
+
+public class AdDealsWallViewModel {
+}
